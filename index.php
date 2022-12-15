83e08013d3b8959d90d1197bbf96851f775c8791
MicheleRai <rainonemichele1999@gmail.com>
2022-12-15 14:52:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+//Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+    $faq = [
+        [
+            'domanda' => '',
+            'risposta' => '',
+        ],
+        [
+            'domanda' => '',
+            'risposta' => '',
+        ],
+        [
+            'domanda' => '',
+            'risposta' => '',
+        ],
+        [
+            'domanda' => '',
+            'risposta' => '',
+        ],
+        [
+            'domanda' => '',
+            'risposta' => '',
+        ],
+
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ</title>
+</head>
+<body>
+    
+</body>
+</html>
